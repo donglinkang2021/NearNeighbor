9c855ee783110f416ab4e5fbf0ec51cdb5a39134
Donglinkang <donglinkang2021@163.com>
2024-07-14 13:28:03 +0800

    add good result

--- demo.py
+++ demo.py
@@ -1,22 +1,52 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 from method import TSPMethod, solve, calculate_total_distance, neighbor_aggregation
-from plot import plot_tsp_solution
+from typing import List, Tuple
+from tqdm import tqdm
 
-# 生成随机城市坐标
-np.random.seed(42)
-num_cities = 30
-cities = np.random.rand(num_cities, 2)
-agg_cities = neighbor_aggregation(cities)
+def solve_tsp(cities: np.ndarray, is_agg:bool = True) -> Tuple[List[int], float]:
+    if is_agg:
+        agg_cities = neighbor_aggregation(cities)
+    else:
+        agg_cities = cities
+    distance_matrix = cdist(agg_cities, agg_cities, metric='euclidean')
+    method = TSPMethod.NEAREST_NEIGHBOR
+    best_tour = solve(distance_matrix, method)
 
-# 计算城市之间的距离矩阵
-distance_matrix = cdist(cities, cities, metric='euclidean')
-agg_distance_matrix = cdist(agg_cities, agg_cities, metric='euclidean')
-
-# 求解
-for method in TSPMethod:
-    best_tour = solve(agg_distance_matrix, method)
+    distance_matrix = cdist(cities, cities, metric='euclidean')
     total_distance = calculate_total_distance(best_tour, distance_matrix)
-    print(f"Best Distance using {method.phrase}: {best_tour}\nTotal Distance: {total_distance:.2f}")
-    plot_tsp_solution(cities, best_tour, total_distance, method.phrase)
+    return best_tour, total_distance
+
+def main():
+    # generate random data
+    np.random.seed(1234)
+    dataset_size = 10000
+    tsp_size = 300
+    data = np.random.uniform(size=(dataset_size, tsp_size, 2)).astype(np.float32)
+
+    pbar = tqdm(total=dataset_size, desc='Processing', dynamic_ncols=True)
+    scale_num = 30
+    result_list = []
+    agg_result_list = []
+    for i in range(dataset_size):
+        cities = data[i][:scale_num]
+
+        best_tour, total_distance = solve_tsp(cities, is_agg=False)
+        result_list.append({'tour': best_tour, 'total_distance': total_distance})
+
+        best_tour, total_distance = solve_tsp(cities, is_agg=True)
+        agg_result_list.append({'tour': best_tour, 'total_distance': total_distance})
+        pbar.update(1)
+    pbar.close()
+    avg_distance = np.mean([result['total_distance'] for result in result_list])
+    print(f'Average distance: {avg_distance}')
+    avg_distance = np.mean([result['total_distance'] for result in agg_result_list])
+    print(f'Average distance with aggregation: {avg_distance}')
+
+if __name__ == '__main__':
+    main()
+
+
+
+    
 
