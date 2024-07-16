861493599ba9cdea7e3f8609b9826aab1ddfdd83
Donglinkang <donglinkang2021@163.com>
2024-07-16 11:44:03 +0800

    add test result

--- gif.py
+++ gif.py
@@ -1,8 +1,12 @@
 from PIL import Image
 import os
-def create_gif(image_folder:str):
+def create_gif(image_folder:str, is_delete:bool=True):
     output_gif = f'{image_folder}/output.gif'
     images = sorted([img for img in os.listdir(image_folder) if img.endswith(".png")])
     frames = [Image.open(os.path.join(image_folder, img)) for img in images]
     frames[0].save(output_gif, save_all=True, append_images=frames[1:], loop=0, duration=500)
     print(f'GIF saved as {output_gif}')
+    if is_delete:
+        for img in images:
+            os.remove(os.path.join(image_folder, img))
+        print(f'Images in {image_folder} are deleted')
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from plot import plot_cities, plot_transform
+from plot import plot_cities, plot_transform, plot_route
 from scipy.spatial.distance import cdist
-from method import solve
 from gif import create_gif 
+from method import solve
 
 def softmax(x:np.ndarray, axis:int=-1) -> np.ndarray:
     exp_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
@@ -14,12 +13,18 @@ def normalize(x:np.ndarray, axis:int=-1, epsilon:float=1e-8) -> np.ndarray:
     std = x.std(axis=axis, keepdims=True)
     return (x - mean) / (std + epsilon)
 
-def cosine_dist(x:np.ndarray) -> np.ndarray:
+def attention_dist(x:np.ndarray) -> np.ndarray:
     return x @ x.T
+
+def cosine_dist(cities:np.ndarray) -> np.ndarray:
+    return cdist(cities, cities, metric='cosine')
+
+def euclidean_dist(cities:np.ndarray) -> np.ndarray:
+    return cdist(cities, cities, metric='euclidean')
 
 def neighbor_aggregation(cities:np.ndarray) -> np.ndarray:
     # note that the input cities should be normalized
-    dist_matrix = cosine_dist(cities)
+    dist_matrix = attention_dist(cities)
     weight = softmax(dist_matrix)
     cities = weight @ cities
     cities = normalize(cities, 0)
@@ -28,16 +33,23 @@ def neighbor_aggregation(cities:np.ndarray) -> np.ndarray:
 def main():
     np.random.seed(1234)
     num_cities = 30
-    cities = np.random.rand(num_cities, 2)
+    cities_origin = np.random.rand(num_cities, 2)
+    distance_matrix_origin = euclidean_dist(cities_origin)
+
+    cities = cities_origin
     for i in range(10):
         cities = normalize(cities, 0)
         plot_cities(cities, title=f'iter={i}')
         transformed_cities = neighbor_aggregation(cities)
         plot_transform(cities, transformed_cities, title=f'iter={i}')
         cities = transformed_cities
-    
-    create_gif('images')
-    # create_gif('images/transform')
+        distance_matrix = euclidean_dist(cities)
+        best_tour = solve(distance_matrix)
+        plot_route(cities_origin, distance_matrix_origin, best_tour, title=f'iter={i}')
+
+    create_gif('images/cities')
+    create_gif('images/transform')
+    create_gif('images/route')
 
 if __name__ == '__main__':
     main()
